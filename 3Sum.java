994881aeadaad3dd9065484c0cbfd47993519dc8
rahulpundhir007 <rahulpundhir07@gmail.com>
2026-01-16 23:33:23 +0530

    [15] [Time Beats: 6.50%] [Memory Beats: 23.85%] - LeetPush

--- 3Sum.java
+++ 3Sum.java
@@ -6,15 +6,15 @@
 6            int st = i+1;
 7            int ed = nums.length-1;
 8            while(st<ed){
-9                ArrayList<Integer> arr = new ArrayList<>();
-10                int sum = nums[i]+nums[st]+nums[ed];
-11                if(sum<0){
-12                    st++;
-13                }
-14                else if(sum>0){
-15                    ed--;
-16                }
-17                else{
+9                int sum = nums[i]+nums[st]+nums[ed];
+10                if(sum<0){
+11                    st++;
+12                }
+13                else if(sum>0){
+14                    ed--;
+15                }
+16                else{
+17                ArrayList<Integer> arr = new ArrayList<>();
 18                    arr.add(nums[i]);
 19                    arr.add(nums[st]);
 20                    arr.add(nums[ed]);
@@ -23,8 +23,8 @@
 23                }
 24            }
 25        }
-26        List<List<Integer>> ans = new ArrayList<>(set);
-27        return ans;
+26        
+27        return new ArrayList<>(set);
 28        }
 29    
 30}
