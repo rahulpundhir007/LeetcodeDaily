55e1ba77cbcb2b4d7179305e01f678864ec0d7ee
rahulpundhir007 <rahulpundhir07@gmail.com>
2026-01-31 18:11:18 +0530

    [2358] [Time Beats: 17.21%] [Memory Beats: 24.59%] - LeetPush

---
+++ Maximum-Number-of-Groups-Entering-a-Competition.java
@@ -0,0 +1,15 @@
+1class Solution {
+2    public int maximumGroups(int[] arr) {
+3        Arrays.sort(arr);
+4        int ans = 0;
+5        int group = 0;
+6        for(int i = 0;i<arr.length;i++){
+7            ans++;
+8            if(ans>group){
+9               group+=1;
+10               ans = 0;
+11            }
+12        }
+13        return group;
+14        }
+15}
