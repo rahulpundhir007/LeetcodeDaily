886be164ea978dc94531f492716416e76a966153
rahulpundhir007 <rahulpundhir07@gmail.com>
2026-02-07 18:39:53 +0530

    [3634] [Time Beats: 22.44%] [Memory Beats: 38.19%] - LeetPush

--- Minimum-Removals-to-Balance-Array.java
+++ Minimum-Removals-to-Balance-Array.java
@@ -2,18 +2,18 @@
 2    public int minRemoval(int[] nums, int k) {
 3        Arrays.sort(nums);
 4        int n = nums.length;
-5        HashMap<Integer,Integer> map = new HashMap<>();
-6        TreeSet<Integer> set = new TreeSet<>();
-7        for(int i = nums.length-1;i>=0;i--){
-8            set.add(nums[i]);
-9            map.put(nums[i],i);
-10        }
-11        int min = Integer.MAX_VALUE;
-12        for(int i = nums.length-1;i>=0;i--){
-13            int ans = (nums[i]+k-1)/k;
-14            int x = set.ceiling(ans);
-15            min = Math.min(min,n-(i-map.get(x)+1));
+5        int ans = nums.length;
+6        int j = 0;
+7        int i = 0;
+8        while(i<nums.length&&j<nums.length){
+9               if((long)nums[i]*k>=nums[j]){
+10                j++;
+11               }
+12               else{
+13                i++;
+14               }
+15            ans = Math.min(ans,n-(j-i));
 16        }
-17       return min;
+17        return ans;
 18    }
 19}
