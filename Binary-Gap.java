d5141be907de7939ebd912951d96d46ceb1a24b9
rahulpundhir007 <rahulpundhir07@gmail.com>
2026-02-24 11:11:42 +0530

    [868] [Time Beats: 100.00%] [Memory Beats: 96.89%] - LeetPush

--- Binary-Gap.java
+++ Binary-Gap.java
@@ -1,16 +1,22 @@
 1class Solution {
 2    public int binaryGap(int n) {
-3        String s = Integer.toBinaryString(n);
-4        int ans = 0;
-5        int idx = -1;
-6        for(int i=0;i<s.length();i++){
-7            if(s.charAt(i)=='1'){
-8                if(idx!=-1){
-9                    ans=Math.max(ans,i-idx);
-10                }
-11                idx=i;
-12            }
-13        }
-14        return ans;
-15    }
-16}
+3       int ans = 1;
+4       int max = 0;
+5       int pre = -1;
+6      while(n!=0){
+7        int m = n%2;
+8        if(m==1&&pre!=-1){
+9            max = Math.max(max,ans);
+10            ans = 1;
+11        }
+12        else if(m==1){
+13            pre = 1;
+14        }
+15        if(pre==1&&m==0){
+16            ans++;
+17        }
+18        n/=2;
+19      }
+20      return max;
+21    }
+22}
