9efb6fd5e37de2291b8c687ea166db6b71f194ac
rahulpundhir007 <rahulpundhir07@gmail.com>
2026-03-11 10:23:13 +0530

    [1009] [Time Beats: 24.44%] [Memory Beats: 82.81%] - LeetPush

--- Complement-of-Base-10-Integer.java
+++ Complement-of-Base-10-Integer.java
@@ -1,23 +1,28 @@
 1class Solution {
 2    public int bitwiseComplement(int n) {
-3        String s  = Integer.toBinaryString(n);
-4        StringBuilder sb = new StringBuilder();
-5        for(int i = 0;i<s.length();i++){
-6            if(s.charAt(i)=='1'){
-7                sb.append("0");
-8            }
-9            else{
-10                 sb.append("1");
-11            }
-12        }
-13        int c= 0;
-14        int ans =0;
-15        for(int i = sb.length()-1;i>=0;i--){
-16            if(sb.charAt(i)=='1'){
-17                ans = ans+ (1<<c);
-18            }
-19            c++;
-20        }
-21        return ans;
-22    }
-23}
+3        StringBuilder sb = new StringBuilder();
+4        if(n==0){
+5            return 1;
+6        }
+7       while(n!=0){
+8        int r = n%2;
+9        if(r==1){
+10            sb.append(0);
+11        }
+12        else{
+13            sb.append(1);
+14        }
+15        n/=2;
+16       }
+17        sb.reverse();
+18        int c= 0;
+19        int ans =0;
+20        for(int i = sb.length()-1;i>=0;i--){
+21            if(sb.charAt(i)=='1'){
+22                ans = ans+ (1<<c);
+23            }
+24            c++;
+25        }
+26        return ans;
+27    }
+28}
